6a277cd8ebbf29d8fd2a8746a9835016754e693f
victorhrls <victorhrls180@gmail.com>
2025-10-02 11:18:58 +0200

    Read me

--- Arrays/3sum.py
+++ Arrays/3sum.py
@@ -8,45 +8,43 @@ Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
 such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
 Approach:
+sort
 """
 
-nums = [-1,0,1,2,-1,-4]
+class Solution:
+    def threeSum(self, nums):
+        triplets =[]
+        size = len(nums)
+        nums.sort()         #easier to work because i can see the extremes
 
-def threeSum(nums):
-    triplets =[]
-    size = len(nums)
-    nums.sort()         #easier to work because i can see the extremes
+        for i in range(size-2):
+                            #i lock the first number
+            j = i+1         # next number
+            k = size -1
+            if i >0 and nums[i] == nums[i-1]:
+                continue # skip everything , not having doubles etc
+            while j<k:
+                first = nums[i]
+                second = nums[j]
+                last = nums[k] # last number
 
-    for i in range(size-2):
-                        #i lock the first number
-        j = i+1         # next number
-        k = size -1
-        if i >0 and nums[i] == nums[i-1]:
-            continue # skip everything , not having doubles etc
-        while j<k:
-            first = nums[i]
-            second = nums[j]
-            last = nums[k] # last number
+                total = first+second+last 
 
-            total = first+second+last 
-
-            if total > 0:
-                k=k-1
-            elif total <0:
-                j = j+1
-            else:
-                triplets.append([first,second,last])
-                j = j +1 
-
-                if j<k and nums[i] == nums[j]:
+                if total > 0:
+                    k=k-1
+                elif total <0:
                     j = j+1
-    
-    return triplets
+                else:
+                    triplets.append([first,second,last])
+                    j = j +1 
+
+                    if j<k and nums[i] == nums[j]:
+                        j = j+1
+        
+        return triplets
 
 
 
 
 
-a= threeSum(nums)
-
-print(a)
+        
--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 Tracking my progress solving LeetCode problems, organized by topic.
 
 ## Arrays
-- [Two Sum](arrays/two_sum.py)
+- [Two Sum](two_sum.py) - Easy
+- [Contains Duplicate](contains_duplicate.py) - Easy
+- [3Sum](3sum.py) - Medium
