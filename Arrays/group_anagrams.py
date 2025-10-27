f4b6d5506617070da24f904c3ba6f7dd5c67e39b
victorhrls <victorhrls180@gmail.com>
2025-10-27 11:30:40 +0100

    ideas

--- Arrays/group_anagrams.py
+++ Arrays/group_anagrams.py
@@ -9,6 +9,9 @@ Given an array of strings strs, group the anagrams together. You can return the 
 Approach:
 """
 
+
+# I can solve it with key , without needing to check every pair (brutal force)
+
 # strs = [""]
 
 
@@ -16,52 +19,68 @@ Approach:
 strs = ["eat","tea","tan","ate","nat","bat"]
 
 
-for ch in strs:
-    print(ch)
-    print(type(ch))
 
-    print()
-    sort = ''.join(sorted(ch))
-    print(sort)
-    print()
+string = 'bolo'
 
+print(string)
+
+
+sorted = ''.join(sorted(string))
+
+print(sorted)
 
 print()
 
-
 def is_anagram(list_of_words):
+
 
     # sort everything out 
 
     out =[]
 
     for ch in list_of_words:
-        element_1 = list_of_words[ch]
-        sort_1 = ''.join(sorted(list_of_words[ch]))
-
-        print(sort_1)
 
 
-        #element = []
+        check = []
 
-        #element.append(element_1)
-        '''
-        for j in range(i+1,list_of_words):
-            element_2 = list_of_words[i+1]
-
-            sort_2 = ''.join(sorted(list_of_words[i+1]))
-
-            if sort_1 == sort_2 :  element.append(element_2)
-            else: continue
-        out.append(element)
-         '''
-
-    
-    #return element
         
 
-#is_anagram(strs)
+        word_of_test = ''.join(sorted(ch))
+
+        check.append(ch)
+
+        #print(word_of_test)
+
+        print(f'A palavra eh {ch} ,ela sorted eh {word_of_test} ')
+        # Check 2 by 2 if there is any anagram
+        # If it is anagram, add to the list
+
+        print()
 
 
-# Check if word 1 and word 2 are equal 
+        for candidate in list_of_words:
 
+            if candidate!= word_of_test: # skip first element
+                sort = ''.join(sorted(candidate))
+
+                print(word_of_test, candidate, sort)
+
+
+                if word_of_test == sort:
+                    check.append(candidate)
+                else:
+                    continue
+        
+        print(check)
+
+        print()
+
+        out.append(check)
+
+        print(out)
+
+
+                
+        
+
+is_anagram(strs)
