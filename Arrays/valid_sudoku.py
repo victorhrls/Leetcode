bc6d41c79ac80cdac2f2fe436cec12400c176cbe
victorhrls <victorhrls180@gmail.com>
2025-10-28 18:41:10 +0100

    sudoku updates

--- Arrays/valid_sudoku.py
+++ Arrays/valid_sudoku.py
@@ -16,6 +16,8 @@ Note: A board does not need to be full or be solvable to be valid.
 
 
 Approach:
+
+# try using set later
 """
 import numpy as np
 
@@ -55,38 +57,30 @@ ex2 =[["8","3",".",".","7",".",".",".","."]
     ,[".",".",".",".","8",".",".","7","9"]]
 
 
-ex3 =[['8','3','.',
-       '6','.','.'
-       '.','9','8']]
-
+ex3 = [
+    ['8', '3', '.'],
+    ['6', '.', '.'],
+    ['.', '9', '8']
+]
 
 # There are 9 blocks 3x3 to verify
 
 
 
 
-def valid_sudoku(matrix):
+def valid_sudoku_block(matrix,size = 3):
     # To verify each rule i should work with SETS
-
     # row r , column c -> Run through (r,c)
-
     # When it will be a subset?  -> Row = 0,3,6 / Column = 0,3,6
-
-
     # First attempt -> Just one set of 9x9 and verify
-
     # What we have to VERIFY
 
-
     rule = {str(x):0 for x in np.arange(1,10)}
-
-
     
-    for row in range(3):
-        for col in range(3):
+    for row in range(size):
+        for col in range(size):
 
             element = matrix[row][col]
-
             # Is it empty ?
 
             if element != '.': 
@@ -94,12 +88,53 @@ def valid_sudoku(matrix):
                     return False
                 else:
                     rule[element] +=1
+    return True
+            
+
+def valid_row(matrix_row,size = 9):
+
+    # the row is fixed
+    row_rules = { str(i) : 0 for i in np.arange(1,10)}
+    for col in range(size): # so iterate EACH col and see what happens
+        element = matrix_row[matrix_row][col]
+
+        if element != '.':
+            if row_rules[element] == 1: return False
+            else: row_rules[element] +=1
+    return True
+
+def valid_col(matrix_col,size = 9):
+
+    col_rules = {str(i):0 for i in np.arange(1,10)}
+    for row in range(size):
+        element = matrix_col[row][matrix_col]
+
+        if element != '.':
+            if col_rules[element] == 1 : return False
+            else : col_rules[element] +=1
 
     return True
-                
 
 
-valid_sudoku(ex3)
+    
+
+def is_valid_sudoku(matrix):
 
 
+    size = len(matrix)
 
+    for element in range(size): 
+
+        # Check line and column
+
+        col_ele = valid_col
+        row_ele = valid_row
+
+        # Then if the element is in some square ,check the 3rd condition
+        if element %3 == 0 : # so it is a new block
+            block_ele = valid_sudoku_block
+
+        if not col_ele or not row_ele or not block_ele : return False
+
+
+    
