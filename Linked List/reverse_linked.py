a8d4eb4957690b7e21a6b2d0e36aab0a0fa8aad6
victorhrls <victorhrls180@gmail.com>
2025-11-11 12:08:23 +0100

    Linked Lists

---
+++ Linked List/reverse_linked.py
@@ -0,0 +1,63 @@
+"""
+LeetCode  - Reverse Linked List
+Difficulty: Easy
+https://leetcode.com/problems/reverse-linked-list/
+
+Problem:
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Approach:
+iteratively and recursively
+"""
+
+class Node:
+    def __init__(self,val=0 ):
+        self.val = val
+        self.next = None
+
+class Linked_List:
+    def __init__(self):
+        self.head = Node()
+        
+    def append(self,value):
+        new_node = Node(value)
+        cur = self.head
+        
+        while cur.next != None: # i get to the END of the list
+            cur = cur.next         
+            
+        cur.next = new_node
+    def size(self):
+        
+        count = 0
+        cur = self.head
+        
+        while cur.next != None:
+            count +=1
+            cur = cur.next
+        
+        return count
+    def view(self):
+        
+        view = []
+        cur = self.head
+        
+        while cur.next != None:
+            cur = cur.next
+            view.append(cur.val)
+        
+        print(view)
+            
+# The head is already a linked list
+    
+def reverse_list(head):
+    lis = Linked_List()
+    
+    head = Linked_List()
+    
+        
+    
+    lis.view()
+  
+test = [1,2,3,4,5]  
+reverse_list(test)
